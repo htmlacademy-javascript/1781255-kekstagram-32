e9006ecf3437d2f5ee15f12e9c7cb4205cee48f6
Busheva_Maria <bushevamv@mail.ru>
2024-07-25 14:44:43 +0300

    Исправлены файлы comments.js и full-photo.js

--- js/comments.js
+++ js/comments.js
@@ -1,23 +1,43 @@
 const commentListElement = document.querySelector('.social__comments');
 const commentElement = document.querySelector('.social__comment');
+const commentsLoaderElement = document.querySelector('.comments-loader');
+const commentsShownElement = document.querySelector('.social__comment-shown-count');
+const commentsTotalElement = document.querySelector('.social__comment-total-count');
+const COMMENTS_PER_PORTION = 5;
+let comments = [];
+let commentsShown = 0;
 
-const getComment = ({avatar, name, message}) => {
-  const comment = commentElement.cloneNode(true);
-  const socialPictureElement = comment.querySelector('.social__picture');
-  socialPictureElement.src = avatar;
-  socialPictureElement.alt = name;
-  comment.querySelector('.social__text').textContent = message;
-  return comment;
-};
-
-const displayComments = (comments) => {
-  const commentsFragmentElement = document.createDocumentFragment();
-  comments.forEach((element) => {
-    const newComment = getComment(element);
-    commentsFragmentElement.append(newComment);
+const addComments = () => {
+  const commentsElement = comments.slice(commentsShown, commentsShown + COMMENTS_PER_PORTION);
+  const commentsElementLength = commentsElement.length + commentsShown;
+  commentsElement.forEach(({avatar, name, message}) => {
+    const commentItemElement = commentElement.cloneNode(true);
+    const pictureElement = commentItemElement.querySelector('.social__picture');
+    pictureElement.src = avatar;
+    pictureElement.alt = name;
+    commentItemElement.querySelector('.social__text').textContent = message;
+    commentListElement.append(commentItemElement);
   });
-  commentListElement.innerHTML = '';
-  commentListElement.append(commentsFragmentElement);
+  commentsShownElement.textContent = `${commentsElementLength}`;
+  commentsTotalElement.textContent = `${comments.length}`;
+  if (comments.length <= commentsElementLength) {
+    commentsLoaderElement.classList.add('hidden');
+  }
+  commentsShown += COMMENTS_PER_PORTION;
 };
 
-export {displayComments};
+const clearComments = () => {
+  while (commentListElement.firstChild) {
+    commentListElement.removeChild(commentListElement.firstChild);
+  }
+  commentsShown = 0;
+  commentsLoaderElement.classList.remove('hidden');
+};
+
+const displayComments = (сomments) => {
+  comments = сomments;
+  addComments();
+  commentsLoaderElement.addEventListener('click', addComments);
+};
+
+export {displayComments, clearComments};
--- js/full-photo.js
+++ js/full-photo.js
@@ -1,14 +1,10 @@
 import {escapeKey} from './util.js';
-import {displayComments} from './comments.js';
+import {clearComments, displayComments} from './comments.js';
 
 const createBigPhoto = (listPictures) => {
   const bigPictureElement = document.querySelector('.big-picture');
   const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
-  const commentCountElement = document.querySelector('.social__comment-count');
-  const commentLoaderElement = document.querySelector('.comments-loader');
   const containerPhotoElement = document.querySelector('.pictures');
-  commentCountElement.classList.add('hidden');
-  commentLoaderElement.classList.add('hidden');
 
   const onDocumentEscKeydown = (evt) => {
     if (escapeKey(evt)) {
@@ -33,10 +29,9 @@ const createBigPhoto = (listPictures) => {
     const {url, likes, comments, description} = listPictures[index];
     bigPictureElement.querySelector('.big-picture__img img').src = url;
     bigPictureElement.querySelector('.likes-count').textContent = likes;
-    bigPictureElement.querySelector('.social__comment-shown-count').textContent = comments.length;
-    bigPictureElement.querySelector('.social__comment-total-count').textContent = comments.length;
     bigPictureElement.querySelector('.social__caption').textContent = description;
-    displayComments(listPictures[index].comments);
+    clearComments();
+    displayComments(comments);
   };
 
   const onClickPhoto = (evt) => {
