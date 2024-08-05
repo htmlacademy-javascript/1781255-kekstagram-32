cd188e6c933ff123516c9ca0f3ba21b892883417
Busheva_Maria <bushevamv@mail.ru>
2024-08-05 18:39:31 +0300

    Добавлены файлы api.js и message.js

--- js/util.js
+++ js/util.js
@@ -1,18 +1,13 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (items) =>
-  items[getRandomInteger(0, items.length - 1)];
-
-const createIdGenerator = () => {
-  let numberId = 0;
-  return () => ++numberId;
+const ALERT_SHOW_TIME = 5000;
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
 };
 
 const escapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomInteger, getRandomArrayElement, createIdGenerator, escapeKey};
+export {showAlert, escapeKey};
